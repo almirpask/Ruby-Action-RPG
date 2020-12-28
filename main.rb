92a8553f03d85da359302225b5ce972c074fed88
Almir Santos <almirpask@hotmail.com>
2020-12-28 06:56:07 -0300

    Add Enemy to main

--- main.rb
+++ main.rb
@@ -3,18 +3,19 @@ require_relative 'lib/graphics/background'
 require_relative 'lib/graphics/tree'
 require_relative 'lib/graphics/grass'
 require_relative 'lib/logic/player'
+require_relative 'lib/logic/enemy'
 require_relative 'lib/logic/collision_manager'
 
 set title: 'Action RPG', fullscreen: false, resizable: false, width: 1280, height: 720, viewport_width: 320, viewport_height: 180, background: 'white'
+
 collision_manager = Logic::CollisionManager.new
 collision_manager.add_object Graphics::Tree.new(x: 55, y: 21)
 collision_manager.add_object Graphics::Tree.new(x: 110, y: 50)
 collision_manager.add_object Graphics::Tree.new(x: 210, y: 50)
 collision_manager.add_object Graphics::Tree.new(x: 150, y: 90)
 collision_manager.add_object Graphics::Grass.new(x: 0, y: 0)
-
+collision_manager.add_enemy Logic::Enemy.new
 Graphics::Background.new(get(:window))
-
 @player = Logic::Player.new
 
 on :key_up do |event|
@@ -39,6 +40,7 @@ update do
   collision_manager.destructible? @player
   @player.is_colliding = collision
   ysort = collision_manager.ysort? @player.sprite.collision
+  collision_manager.player_detection_zone? @player.sprite.collision
   @player.sprite.z = if ysort
                        4
                      else
