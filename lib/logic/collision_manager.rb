5d1f5db424e4dc04a6e60ab10a87bd4c88982eef
Almir Santos <almirpask@hotmail.com>
2020-12-28 04:17:21 -0300

    Add destructible behavior to sprites

--- lib/logic/collision_manager.rb
+++ lib/logic/collision_manager.rb
@@ -1,9 +1,10 @@
 class Logic::CollisionManager
-  attr_accessor :collisions, :ysorts
+  attr_reader :collisions, :ysorts, :destructibles
 
   def initialize
     @collisions = []
     @ysorts = []
+    @destructibles = []
   end
 
   def collision?(player)
@@ -14,9 +15,14 @@ class Logic::CollisionManager
     check_overlap @ysorts, player
   end
 
+  def destructible?(player)
+    check_overlap_and_destroy @destructibles, player.sprite.hit_box if player.state == :atack
+  end
+
   def add_object(object)
-    @collisions << object.collision unless object.collision.nil?
-    @ysorts << object.ysort unless object.ysort.nil?
+    @collisions << object.collision if object.respond_to? :collision
+    @destructibles << object if object.respond_to?(:collision) && object.destructible
+    @ysorts << object.ysort if object.respond_to? :ysort
   end
 
   private
@@ -24,6 +30,16 @@ class Logic::CollisionManager
   def check_overlap(objects, player)
     Array(objects).any? do |object|
       horizontal_overlap(player, object) && vertical_overlap(player, object)
+    end
+  end
+
+  def check_overlap_and_destroy(objects, hit_box)
+    objects.each do |object|
+      has_overlap = horizontal_overlap(hit_box, object.collision) && vertical_overlap(hit_box, object.collision)
+      if has_overlap
+        object.destroy
+        object.collision.remove
+      end
     end
   end
 
