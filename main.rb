926acb2e1b4bb3777f843645dcf68aebad22f7d0
Almir Santos <almirpask@hotmail.com>
2020-12-30 04:39:00 -0300

    Add Grass Logic

--- main.rb
+++ main.rb
@@ -1,32 +1,28 @@
 require 'ruby2d'
 require_relative 'lib/graphics/background'
 require_relative 'lib/graphics/tree'
-require_relative 'lib/graphics/grass'
 require_relative 'lib/graphics/Hearth'
 require_relative 'lib/logic/player'
 require_relative 'lib/logic/enemy'
 require_relative 'lib/logic/collision_manager'
+require_relative 'lib/logic/grass'
 
 set title: 'Action RPG', fullscreen: false, resizable: false, width: 1280, height: 720, viewport_width: 320, viewport_height: 180, background: 'white'
 SHOW_COLLISIONS = 1 # 1 TRUE, 0 FALSE
 collision_opacity = 0
-
-case SHOW_COLLISIONS
-when 0
-  collision_opacity = 0
-when 1
-  collision_opacity = 0.5
-end
+collision_opacity = 0.5 if SHOW_COLLISIONS == 1
 
 collision_manager = Logic::CollisionManager.new
 collision_manager.add_object Graphics::Tree.new(x: 55, y: 21, collision_opacity: collision_opacity)
 collision_manager.add_object Graphics::Tree.new(x: 110, y: 50, collision_opacity: collision_opacity)
 collision_manager.add_object Graphics::Tree.new(x: 210, y: 50, collision_opacity: collision_opacity)
 collision_manager.add_object Graphics::Tree.new(x: 150, y: 90, collision_opacity: collision_opacity)
-collision_manager.add_object Graphics::Grass.new(x: 0, y: 0, collision_opacity: collision_opacity)
+collision_manager.add_object Logic::Grass.new(x: 0, y: 0, collision_opacity: collision_opacity)
 
 enemies = []
-enemies << Logic::Enemy.new(collision_opacity: collision_opacity)
+enemies << Logic::Enemy.new(collision_opacity: collision_opacity, x: 100, y: 100)
+enemies << Logic::Enemy.new(collision_opacity: collision_opacity, x: 200, y: 130)
+enemies << Logic::Enemy.new(collision_opacity: collision_opacity, x: 34, y: 70)
 enemies.each do |enemy|
   collision_manager.add_enemy enemy
 end
